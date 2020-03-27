9f704cb0845014eab72921f8ac14608eae7638fa
shalvah <diakon.ng@gmail.com>
2020-03-27 09:23:17 +0100

    Add funcs for aws CLI

--- profile/custom.ps1
+++ profile/custom.ps1
@@ -2,6 +2,7 @@ function global:Set-Kube ([String] $proj) {
     $Map = @{
         "hc-dev"  = "arn:aws:eks:eu-west-1:563178737752:cluster/HC-DevStage-App-EKS";
         "hc-prod" = "arn:aws:eks:eu-west-1:563178737752:cluster/HC-Production-App-EKS";
+        "orderin-prod" = "arn:aws:eks:eu-west-1:843212092188:cluster/kroc-cluster";
     }
 
     kubectl config use-context $Map[$proj]
@@ -14,9 +15,16 @@ function global:Get-Kube () {
 
 function global:Set-Gcloud ([String] $proj) {
     gcloud config set project $proj
-
 }
 
 function global:Get-Gcloud () {
     gcloud config get-value project
+}
+
+function global:Set-Aws ([String] $proj) {
+    setx AWS_PROFILE $proj
+}
+
+function global:Get-Aws () {
+    aws configure list
 }
