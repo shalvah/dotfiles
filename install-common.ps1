cd7b692a1a1ca63a5baa4940d434f0613ae5f4a9
Shalvah <shalvah@users.noreply.github.com>
2022-02-13 18:02:14 +0100

    Update install-common.ps1

--- install-common.ps1
+++ install-common.ps1
@@ -2,12 +2,15 @@
 Set-ExecutionPolicy Bypass
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')) -ErrorAction Stop;
 
-choco install -y nvm.portable insomnia-rest-api-client
+choco install -y insomnia-rest-api-client
+# Install nvm from https://github.com/coreybutler/nvm-windows/releases
 choco install -y jetbrainstoolbox
 choco install -y git.install --params '"/GitOnlyOnPath /NoShellIntegration"'    
 choco install -y php --params '"/ThreadSafe /InstallDir:C:\PHP"'
 # Don't forget to enable PHP extensions: curl, dom, exif, fileinfo, gd, intl, json, mysqli, pdo_mysql, pdo_pgsql, pdo_sqlite
-# Install redis, xdebug
+# Install xdebug. Settings:
+# zend_extension = php_xdebug-3.1.2-8.1-vs16-x86_64.dll
+# xdebug.mode = coverage;
 # openssl and mbstring may already be enabled
 
 choco install -y composer ngrok
