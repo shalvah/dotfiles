e0b9855554e7a4985980a2e9936285c1ac22c266
Shalvah <shalvah@users.noreply.github.com>
2023-02-19 00:22:13 +0100

    Update install-personal.ps1

--- install-personal.ps1
+++ install-personal.ps1
@@ -1,10 +1,9 @@
 # Utilities
 choco install -y vlc 7zip screentogif
-choco install -y google-backup-and-sync --ignore-checksums
 
 choco install -y origin steam
 choco install -y activepresenter
 # Install manually:
-# Google Drive, Unified Remote, FDM, VSCode, Docker for Windows, Google Cloud SDK, Xodo PDF Reader (from MS Store)
+# Google Drive, Unified Remote, FDM, Xodo PDF Reader (from MS Store)
 
-# Ray (http://myray.app), QTTabBar
+# Install WinCompose http://wincompose.info/
